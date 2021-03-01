d6e886a9978b18829d7ac41af203edb822d85362
genesisbertiz <gnbertiz08@gmail.com>
2021-03-01 16:25:41 +0800

    1 file modified

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,25 +1,37 @@
-import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostBinding, HostListener, Input, OnChanges, OnInit, ViewChild } from "@angular/core";
-import { TweenLite } from 'gsap'
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnChanges,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+//import { TweenLite } from 'gsap'
 import { MenusInterface } from "src/app/app.component";
-import * as _ from 'lodash'
+import * as _ from "lodash";
 @Component({
   selector: "app-dropdown",
   templateUrl: "./dropdown.component.html",
-  styleUrls: ["./dropdown.component.scss"],
+  styleUrls: ["./dropdown.component.scss"]
 })
-export class DropdownComponent implements AfterViewInit, OnChanges, AfterViewChecked {
-  @ViewChild('labelContainer') labelContainer: ElementRef;
+export class DropdownComponent
+  implements AfterViewInit, OnChanges, AfterViewChecked {
+  @ViewChild("labelContainer") labelContainer: ElementRef;
   @Input() data: MenusInterface[];
   @Input() elements: any[];
   dataPass: MenusInterface[] = [];
   showContent = false;
-  constructor(private cd: ChangeDetectorRef) {
-  }
+  constructor(private cd: ChangeDetectorRef) {}
   ngAfterViewChecked() {
-    this.dataPass = _.sortBy(this.data, (e) => e.id)
+    this.dataPass = _.sortBy(this.data, e => e.id);
   }
   ngOnChanges(changes) {
-    this.dataPass = _.sortBy(this.data, (e) => e.id)
+    this.dataPass = _.sortBy(this.data, e => e.id);
   }
   ngAfterViewInit(): void {
     // console.log(this.data)
